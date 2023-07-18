aac8ab41d8697fc981341a0e846f31bf33acf037
yossiarar99 <yossef.arar@qualitestgroup.com>
2023-07-18 14:05:58 +0300

    first commit

--- app/main_app.py
+++ app/main_app.py
@@ -67,6 +67,15 @@ class App:
                             self.__st.error(f"The file path: {value} not exists.")
         self.__certificate_update[key] = value
 
+    def __convert_docx_to_markdown(self, input_file, output_file=None):
+        self.__doc = Document(input_file)
+        paragraphs = [p.text for p in self.__doc.paragraphs]
+        markdown_text = '\n'.join(paragraphs)
+        output_file = output_file if output_file else input_file.replace('.docx', '_temp.md')
+        with open(output_file, 'w', encoding='utf-8') as f:
+            f.write(markdown_text)
+        return f, markdown_text
+
     def __initiate_ui_field_options(self, sidebar: bool):
         ui_fields = {}
         for key, values in self.__config.items():
@@ -95,46 +104,47 @@ class App:
             p.paragraph_format.left_indent = 1
             p.paragraph_format.keep_together = True
             for field, field_value in self.__certificate_update.items():
-                field_key, line_field_margin = '{' + field + '}', 0
-                # field_key, line_field_margin = field, 0
-                if field_key in p.text:
-                    inline = p.runs
-                    # Loop added to work with runs (strings with same style)
-                    for i in range(len(inline)):
-                        if field_key in inline[i].text:
+                if field_value is not None and len(str(field_value)) > 1:
+                    field_key, line_field_margin = '{' + field + '}', 0
+                    # field_key, line_field_margin = field, 0
+                    if field_key in p.text:
+                        inline = p.runs
+                        # Loop added to work with runs (strings with same style)
+                        for i in range(len(inline)):
                             if field_key in inline[i].text:
-                                text = inline[i].text.replace(field_key, str(field_value))
-                                # if len(text) != MAX_CHARS:
-                                if len(text) > 0:
-                                    split_values = text.split('  ')
-                                    if len(split_values) > 0:
-                                        start_p, index = None, 0
-                                        # found start p value
-                                        for val in split_values:
-                                            index += 1
-                                            if str(field_value) in val:
-                                                start_p = str(val[(0 if str(val)[0] != ' ' else 1):(
-                                                    -1 if str(val)[-1] == ' ' else len(val))])
-                                                break
-                                        if start_p:
-                                            end_p, field_value_len = '', len(field_key)
-                                            for val in split_values[index:]:
+                                if field_key in inline[i].text:
+                                    text = inline[i].text.replace(field_key, str(field_value))
+                                    # if len(text) != MAX_CHARS:
+                                    if len(text) > 0:
+                                        split_values = text.split('  ')
+                                        if len(split_values) > 0:
+                                            start_p, index = None, 0
+                                            # found start p value
+                                            for val in split_values:
                                                 index += 1
                                                 if str(field_value) in val:
-                                                    end_p = str(val[(0 if str(val)[0] != ' ' else 1):(
+                                                    start_p = str(val[(0 if str(val)[0] != ' ' else 1):(
                                                         -1 if str(val)[-1] == ' ' else len(val))])
                                                     break
-                                            space_margin, space_value = PART1_LEN - (len(start_p) + len(end_p)), ''
-                                            while space_margin != 0:
-                                                space_value += ' '
-                                                space_margin = space_margin - 1
-                                            part1 = f"{start_p}{space_value}"
-                                            print(f"p1 = {len(part1)}, {field_value_len}")
-                                            text = f"{part1}{end_p}"
-                                            if len(text) > MAX_CHARS:
-                                                text = text.replace(space_value[:len(text) - MAX_CHARS], '', 1)
-                                inline[i].text = text
-                                print(text, len(text))
+                                            if start_p:
+                                                end_p, field_value_len = '', len(field_key)
+                                                for val in split_values[index:]:
+                                                    index += 1
+                                                    if str(field_value) in val:
+                                                        end_p = str(val[(0 if str(val)[0] != ' ' else 1):(
+                                                            -1 if str(val)[-1] == ' ' else len(val))])
+                                                        break
+                                                space_margin, space_value = PART1_LEN - (len(start_p) + len(end_p)), ''
+                                                while space_margin != 0:
+                                                    space_value += ' '
+                                                    space_margin = space_margin - 1
+                                                part1 = f"{start_p}{space_value}"
+                                                print(f"p1 = {len(part1)}, {field_value_len}")
+                                                text = f"{part1}{end_p}"
+                                                if len(text) > MAX_CHARS:
+                                                    text = text.replace(space_value[:len(text) - MAX_CHARS], '', 1)
+                                    inline[i].text = text
+                                    print(text, len(text))
             if pi == 1 and self.__certificate_update.get('Image Path'):
                 add_float_picture(p=p, image_path_or_stream=self.__certificate_update.get('Image Path'),
                                   width=Inches(self.__certificate_update.get('image_data').get('width')),
@@ -145,8 +155,16 @@ class App:
         doc_path, doc_name = f"{os.getcwd()}/files/certificates/", f"{self.__certificate_update['Date']}_{self.__certificate_update['Certificate number']}.docx"
         doc_full_path = f"{doc_path}{doc_name}"
         self.__doc.save(doc_full_path)
-        with open(doc_full_path, 'rb') as d:
-            data = d.read()
+
+        # # check option for present word document
+        # with open(doc_full_path, 'rb') as d:
+        #     data = d.read()
+        # markdown_text, doc = self.__convert_docx_to_markdown(input_file=doc_full_path), self.__doc
+        # markdown_file = doc_full_path.replace('.docx', '_temp.md')
+        # with open(markdown_file, 'w', encoding='utf-8') as f:
+        #     f.write(markdown.markdown(str(data)))
+        # st.markdown(doc.paragraphs, unsafe_allow_html=False)
+
         download_button = self.__st.download_button(label='Download Certificate', data=data, file_name=doc_name)
         if download_button:
             self.__st.success("Your new certificate is ready to use")
